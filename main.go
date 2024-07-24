f4610c31a153f16b1446c3486496d3bcda3dbeb3
Apichon.C <apichonbxss@gmail.com>
2024-07-24 11:14:41 +0700

    first bug

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"net"
+)
+
+const defaultListenAddr = ":6666"
+
+type Config struct {
+	ListenAddr string
+}
+
+type Server struct {
+	Config
+	peers     map[*Peer]bool
+	ln        net.Listener
+	addPeerCh chan *Peer
+}
+
+func NewServer(cfg Config) *Server {
+	if len(cfg.ListenAddr) == 0 {
+		cfg.ListenAddr = defaultListenAddr
+	}
+	return &Server{
+		Config:    cfg,
+		peers:     make(map[*Peer]bool),
+		addPeerCh: make(chan *Peer),
+	}
+}
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.ListenAddr)
+	if err != nil {
+		return err
+	}
+	s.ln = ln
+	go s.loop()
+
+	return s.acceptLoop()
+
+}
+func (s *Server) loop() {
+	for {
+		select {
+		case peer := <-s.addPeerCh:
+			s.peers[peer] = true
+		default:
+			fmt.Println("foo")
+		}
+	}
+}
+func (s *Server) acceptLoop() error {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			slog.Error("accept error", "err", err)
+			continue
+		}
+		go s.handleConn(conn)
+	}
+}
+
+func (s *Server) handleConn(conn net.Conn) {
+	peer := NewPeer(conn)
+	s.addPeerCh <- peer
+
+	go peer.readLoop()
+}
+
+func main() {
+
+}
